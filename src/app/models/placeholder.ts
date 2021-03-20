12019851c46bc7d89d196cd271c44e2fdce1b941
Amen Alahassa <am.konrad21@gmail.com>
2021-03-20 22:31:24 +0100

    Creation de placeholder

---
+++ src/app/models/placeholder.ts
@@ -0,0 +1,55 @@
+
+export const SurveyListItems = [
+  {
+    name: 'ISO 2000',
+    update: '2019-12-03'
+  },
+  {
+    name: 'ACMA FORMS',
+    update: '2019-07-27'
+  },
+  {
+    name: 'BIO CERTIFICATE',
+    update: '2019-12-03'
+  },
+  {
+    name: 'DANDNE CERTIFICATE',
+    update: '2019-12-03'
+  },
+  {
+    name: 'ISO 3050',
+    update: '2019-12-03'
+  },
+];
+
+
+// export const ToolBoxItem = {
+//   forms : [
+//     {
+//       name : 'New Form',
+//       logo: 'insert_drive_file',
+//     },
+//     {
+//       name : 'ID',
+//       logo: 'address-card',
+//     },
+//     {
+//       name : 'Adress',
+//       logo: 'place',
+//     },
+//     {
+//       name : 'Contact',
+//       logo: 'insert_drive_file'
+//     },
+//     {
+//       name : 'Skills',
+//       logo: 'insert_drive_file'
+//     },
+//   ],
+//   elements: [
+//   {
+//     name : 'Text',
+//     logo: ''
+//   }
+//   ]
+// };
--- src/app/service/survey.service.ts
+++ src/app/service/survey.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import {SurveyModels} from '../models/survey.models';
 import {Subject} from 'rxjs';
+import {SurveyListItems} from '../models/placeholder';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SurveyService {
 
-  surveys: SurveyModels[] = [];
+  surveys: SurveyModels[] = SurveyListItems;
   surveysSubject = new Subject<SurveyModels[]>();
 
   constructor() { }
